d53bc9b8924a22a5f9981e5247e614a7e77d0ccb
Marco Barraza <marko.barraza@gmail.com>
2018-08-19 04:01:13 -0300

    Se agrego cover y funcionalidades JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// Modal img section inspiracion
 $(function(){
 	$(".inspiracion__big").attr("src", "https://picsum.photos/500/500?image=3");
 
@@ -7,4 +7,20 @@ $(function(){
 		$(".inspiracion__big").attr("src", click);
 
 	})
+})
+
+// $(function(){
+// 	$("a[href='#top']").on("click", function(){
+// 		$("html, body").animate({ scrollTop: auto }, "slow");
+//   		return false;
+// 	})
+// })
+
+// Scroll to...
+$(function(){
+	$(".cover__icon").on("click", function() {
+	    $('html, body').animate({
+	        scrollTop: $("#top").offset().top
+	    }, 1000);
+	});
 })
